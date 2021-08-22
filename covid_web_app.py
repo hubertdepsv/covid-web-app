786f1e744367c438bd4b9b3676b9a4b1f555070b
hubertdepsv <60505590+hubertdepsv@users.noreply.github.com>
2021-08-22 16:52:40 +0200

    Add files via upload

---
+++ covid_web_app.py
@@ -0,0 +1,202 @@
+# setup a tracking dashboard
+
+# import necessary libraries
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import pandas as pd
+from datetime import datetime
+
+# request data from the esri website
+raw = requests.get('https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/1/query?where=1%3D1&outFields=*&outSR=4326&f=json')
+raw_json = raw.json()
+df = d.DataFrame(raw_json["features"])
+
+# in the case of COVID record we have geographical attributes, numbers and a timestamp
+
+# transforming data
+data_list = df['attributes'].tolist()
+df_final = pd.DataFrame(data_list)
+df_final = df_final[["Country_Region", "Province_State", "Lat", "Long_", "Confirmed", "Deaths", "Recovered", "Last_Update"]]
+
+# cleaning data
+# missing values in the Last_Update and Province_State columns
+# Last_Update column values are timestamps in milliseconds
+
+def convert_time(t):
+    t = int(t)
+    return datetime.fromtimestamp(t)
+    
+df_final = df_final.dropna(subset=['Last_Update'])
+df_final['Province_State'].fillna(value='', inplace=True)
+
+df_final['Last_Update'] = df_final['Last_Update'] / 1000
+df_final['Last_Update'] = df_final['Last_Update'].apply(convert_time)
+
+# aggregating data
+df_total = df_final.groupby("Country_Region", as_index=False).agg(
+    {
+        "Confirmed" : "sum",
+        "Deaths" : "sum",
+        "Recovered" : "sum"
+    }
+)
+
+# calculating daily total of COVID-19 cases
+total_confirmed = df_final["Confirmed"].sum()
+total_recovered = df_final["Recovered"].sum()
+total_deaths = df_final["Deaths"].sum()
+
+df_top10 = df_total.nlargest(10, "Confirmed")
+top10_countries_1 = df_top10["Country_Region"].tolist()
+top10_confirmed = df_top10["Confirmed"].tolist()
+
+df_top10 = df_total.nlargest(10, "Recovered")
+top10_countries_2 = df_top10["Country_Region"].tolist()
+top10_recovered = df_top10["Recovered"].tolist()
+
+df_top10 = df_total.nlargest(10, "Deaths")
+top10_countries_3 = df_top10["Country_Region"].tolist()
+top10_deaths = df_top10["Deaths"].tolist()
+
+# building a dashboard using plotly subplots
+# our dashboard will display the daily total of confirmed, recovered and death cases worldwide
+# the same totals for each country
+# top 10 countries for each total
+
+# initializing the subplots
+fig = make_subplots(
+    rows=4, cols=6,
+    specs=[
+        [{"type": "scattergeo", "rowspan": 4, "colspan": 3}, None, None, {"type": "indicator"}, {"type": "indicator"}],
+        [None, None, None, {"type": "bar", "colspan": 3}, None, None],
+        [None, None, None, {"type": "bar", "colspan": 3}, None, None],
+        [None, None, None, {"type": "bar", "colspan": 3}, None, None],
+    ])
+
+# annotating our plots
+message = df_final["Country_region"] + " " + df_final["Province_State"] + "<br>"
+message += "Confirmed: " + df_final["Confirmed"].astype(str) + "<br>"
+message += "Deaths: " + df_final["Deaths"].astype(str) + "<br>"
+message += "Recovered: " + df_final["Recovered"].astype(str) + "<br>"
+message += "Last updated: " + df_final["Last_Update"].astype(str)
+df_final["text"] = message
+
+# scattergeo subplot
+fig.add_trace(
+    go.Scattergeo(
+        locationmode='country names',
+        lon=df_final['Long_'],
+        lat=df_final['Lat'],
+        hovertext=df_final['text'],
+        showlegend=False,
+        marker=dict(
+            size=10,
+            opacity=0.8,
+            autocolorscale=True,
+            symbol='square',
+            line=dict(
+                width=1,
+                color='rgba(102, 02, 102)'
+            ),
+            cmin=0,
+            color=df_final['Confirmed'],
+            cmax=df_final['Confirmed'].max(),
+            colorbar_title='Confirmed Cases<br>Latest Update',
+            colorbar_x=-0.05
+        )
+    ),
+    row=1,
+    col=1
+)
+
+# indicator subplot
+
+fig.add_trace(
+    go.Indicator(
+        mode="number",
+        value=total_confirmed,
+        title="Confirmed Cases",
+    ),
+    row=1, col=4
+)
+
+fig.add_trace(
+    go.Indicator(
+        mode="number",
+        value=total_recovered,
+        title="Recovered Cases",
+    ),
+    row=1, col=5
+)
+
+fig.add_trace(
+    go.Indicator(
+        mode="number",
+        value=total_deaths,
+        title="Deaths Cases",
+    ),
+    row=1, col=6
+)
+
+# bar charts
+
+fig.add_trace(
+    go.Bar(
+        x=top10_countries_1,
+        y=top10_confirmed, 
+        name= "Confirmed Cases",
+        marker=dict(color="Yellow"), 
+        showlegend=True,
+    ),
+    row=2, col=4
+)
+
+fig.add_trace(
+    go.Bar(
+        x=top10_countries_2,
+        y=top10_recovered, 
+        name= "Recovered Cases",
+        marker=dict(color="Green"), 
+        showlegend=True),
+    row=3, col=4
+)
+
+fig.add_trace(
+    go.Bar(
+        x=top10_countries_3,
+        y=top10_deaths, 
+        name= "Deaths Cases",
+        marker=dict(color="crimson"), 
+        showlegend=True),
+    row=4, col=4
+)
+
+# finalize layout
+
+fig.update_layout(
+    template="plotly_dark",
+    title = "Global COVID-19 Cases (Last Updated: " + str(df_final["Last_Update"][0]) + ")",
+    showlegend=True,
+    legend_orientation="h",
+    legend=dict(x=0.65, y=0.8),
+    geo = dict(
+            projection_type="orthographic",
+            showcoastlines=True,
+            landcolor="white", 
+            showland= True,
+            showocean = True,
+            lakecolor="LightBlue"
+    ),
+
+    annotations=[
+        dict(
+            text="Source: https://bit.ly/3aEzxjK",
+            showarrow=False,
+            xref="paper",
+            yref="paper",
+            x=0.35,
+            y=0)
+    ]
+)
+
+fig.write_html('first_figure.html', auto_open=True)
